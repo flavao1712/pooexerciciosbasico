b60319581b8ac49aca76b429ec60e283858fe066
Flavio augusto <flaviomedina1712@gmail.com>
2026-03-27 07:09:13 -0300

    cliente

--- src/entrega/cliente.java
+++ src/entrega/cliente.java
@@ -2,6 +2,24 @@ package entrega;
 
 public class cliente {
 
+    public static void main(String[] args) {
 
+        entregashopee joao = new entregashopee();
+        joao.nome = "João";
+        joao.rua = "Jose Sebastião boarder";
+        joao.bairro = "Sion";
+        joao.numero = 145;
+        joao.referencia ="Casa de esquina";
+
+        entregashopee maria = new entregashopee();
+        maria.nome = "Maria";
+        maria.rua = "Luís Pederiva";
+        maria.bairro = "Parque boa vista";
+        maria.numero = 52;
+        maria.referencia ="Muro de cor marron";
+
+
+        System.out.println(maria.rua);
+    }
 
 }
